30e6b845170d7475cd81a5e8c2ba36b3bd4e6087
Michel Perez <michel.ingesoft@gmail.com>
2021-03-02 04:37:31 -0500

    Adding chromosome struct

--- src/framework.rs
+++ src/framework.rs
@@ -1,15 +1,50 @@
-pub type Population<T> = Vec<T>;
+use std::fmt::Debug;
+
+#[derive(Clone, Copy)]
+pub struct Chromosome<T> {
+    pub genes: T,
+    size: i32,
+    fitness: f32,
+    age: i32,
+}
+
+impl<T> Chromosome<T> {
+    pub fn new(genes: T) -> Self {
+        Chromosome {
+            genes: genes,
+            size: 0,
+            fitness: 0.0,
+            age: 0,
+        }
+    }
+}
+
+impl<T> Default for Chromosome<T>
+where
+    T: Default,
+{
+    fn default() -> Chromosome<T> {
+        Chromosome {
+            genes: T::default(),
+            size: 0,
+            fitness: 0.0,
+            age: 0,
+        }
+    }
+}
+
+pub type Population<T> = Vec<Chromosome<T>>;
 
 pub trait IFramework<T> {
     fn genotype(self) -> Population<T>;
-    fn fitness_function(self, cromosome: &T) -> i32;
+    fn fitness_function(self, chromosome: &Chromosome<T>) -> i32;
     fn xover(self, population: &Population<T>) -> Population<T>;
     fn mutate(self, population: &Population<T>) -> Population<T>;
     fn max_iters(self) -> i32;
 }
 
 pub struct FrameworkSolver<T, F: IFramework<T>> {
-    population: Vec<T>,
+    population: Population<T>,
     framework: F,
 }
 
@@ -17,6 +52,7 @@ impl<T, F> FrameworkSolver<T, F>
 where
     T: Clone,
     T: Default,
+    T: Debug,
     F: IFramework<T>,
     F: Copy,
 {
@@ -29,32 +65,34 @@ where
 
     pub fn run(mut self) {
         self.population = self.framework.genotype();
-        let (mut ext_max_val, mut ext_max_elem) = self.best_result();
+        let mut ext_max_elem = self.best_result();
         let mut iter = 0;
         while iter < self.framework.max_iters() {
             iter += 1;
             println!("iter {:?}", iter);
             self.population = self.framework.xover(&self.population);
             self.population = self.framework.mutate(&self.population);
-            let (max_val, max_elem) = self.best_result();
-            ext_max_val = max_val;
+            let max_elem = self.best_result();
             ext_max_elem = max_elem;
         }
         println!("Total iter {:?}", iter);
-        println!("Best val {:?}", ext_max_val);
+        println!("Age {:?}", ext_max_elem.age);
+        println!("Best val {:?}", ext_max_elem.genes);
     }
 
-    fn best_result(&self) -> (i32, T) {
-        self.population.iter().cloned().enumerate().fold(
-            (0, T::default()),
-            |(max_val, max_elem), (i, elem)| {
-                let val: i32 = self.framework.fitness_function(&elem);
-                if val > max_val {
-                    (val, elem)
-                } else {
-                    (max_val, max_elem)
-                }
-            },
-        )
+    fn best_result(&mut self) -> Chromosome<T> {
+        let mut best_chromosome = &Chromosome::default();
+        for chromosome in self.population.iter_mut() {
+            let fitness: i32 = self.framework.fitness_function(&chromosome);
+
+            chromosome.age = chromosome.age + 1;
+            chromosome.fitness = fitness as f32;
+            best_chromosome = if chromosome.fitness > best_chromosome.fitness {
+                chromosome
+            } else {
+                best_chromosome
+            }
+        }
+        best_chromosome.clone()
     }
 }
--- src/one_max.rs
+++ src/one_max.rs
@@ -1,4 +1,4 @@
-use crate::framework::{IFramework, Population};
+use crate::framework::{Chromosome, IFramework, Population};
 use rand::Rng;
 
 #[derive(Clone, Copy)]
@@ -27,30 +27,30 @@ impl IFramework<Vec<i8>> for OneMaxFramework {
             for _ in 0..self.m {
                 row.push(rng.gen_range(0..2));
             }
-            ret.push(row);
+            ret.push(Chromosome::new(row));
         }
         ret
     }
 
-    fn fitness_function(self, cromosome: &Vec<i8>) -> i32 {
-        cromosome.iter().fold(0, |acc, n| acc + *n as i32)
+    fn fitness_function(self, cromosome: &Chromosome<Vec<i8>>) -> i32 {
+        cromosome.genes.iter().fold(0, |acc, n| acc + *n as i32)
     }
 
     fn xover(self, population: &Population<Vec<i8>>) -> Population<Vec<i8>> {
         let mut new_population = population.clone();
         let mut i = 0;
         let mut rng = rand::thread_rng();
-        let row_size = new_population[0].len();
+        let row_size = new_population[0].genes.len();
         while i < new_population.len() {
-            let p1 = new_population[i].clone();
-            let p2 = new_population[i + 1].clone();
+            let p1 = new_population[i].genes.clone();
+            let p2 = new_population[i + 1].genes.clone();
             let break_point = rng.gen_range(0..row_size);
 
             let (p1_1, p1_2) = p1.split_at(break_point);
             let (p2_1, p2_2) = p2.split_at(break_point);
 
-            new_population[i] = [p1_1, p2_2].concat();
-            new_population[i + 1] = [p2_1, p1_2].concat();
+            new_population[i].genes = [p1_1, p2_2].concat();
+            new_population[i + 1].genes = [p2_1, p1_2].concat();
 
             i += 2;
         }
@@ -61,9 +61,9 @@ impl IFramework<Vec<i8>> for OneMaxFramework {
         let mut new_population = population.clone();
         let mut rng = rand::thread_rng();
         for i in 0..new_population.len() {
-            for j in 0..new_population[i].len() {
-                if new_population[i][j] == 0 && rng.gen_range(0..100) < 5 {
-                    new_population[i][j] = rng.gen_range(0..2);
+            for j in 0..new_population[i].genes.len() {
+                if new_population[i].genes[j] == 0 && rng.gen_range(0..100) < 5 {
+                    new_population[i].genes[j] = rng.gen_range(0..2);
                 }
             }
         }
